d7aa5d74ae21d7d17e18e25313fd868694726e1a
zennyx <blinkbaby@163.com>
2020-07-22 23:16:02 +0800

    Add new feature

---
+++ springfield-commons/src/main/java/zenny/toybox/springfield/keyvalue/support/CompositeKeyValueLoader.java
@@ -0,0 +1,15 @@
+package zenny.toybox.springfield.keyvalue.support;
+
+import java.util.Map;
+
+import zenny.toybox.springfield.keyvalue.KeyValueLoader;
+
+public class CompositeKeyValueLoader<K, V> implements KeyValueLoader<K, V> {
+
+  @Override
+  public Map<K, V> load() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
